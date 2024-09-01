583ea35937fd235cb854001e829fb02897c6009f
Thomas <tc12game@gmail.com>
2024-09-01 19:39:51 -0400

    Fixing flask again.

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -24,7 +24,7 @@ def serve():
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
