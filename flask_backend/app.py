ccc3211fbbe8991e71b36dfeed0fe2c6466fb230
Thomas <tc12game@gmail.com>
2024-08-31 23:27:51 -0400

    Fixed postgres.

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -29,6 +29,9 @@ def serve(path):
 
 # Gets database url to create database
 database_url = os.getenv("DATABASE_URL")
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+    
 engine = create_engine(database_url)
 Base = declarative_base()
 
