8dd17e275d865f3a85082bf20d3ceb071c2dae6b
Thomas <tc12game@gmail.com>
2024-10-20 22:22:35 -0400

    Still fixing cloudflare.

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -18,7 +18,13 @@ app = Flask(__name__, static_folder='../book-finder/build', static_url_path='')
 app.secret_key = os.getenv('SECRET_KEY_FLASK') # Not sure
 
 # To connect to react
-CORS(app, resources={r"/*": {"origins": ["https://booksearchapp.com", "https://book-search-4tcm.onrender.com"]}})
+CORS(app, resources={
+    r"/*": {
+        "origins": ["https://booksearchapp.com", "https://book-search-4tcm.onrender.com"],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"]
+    }
+})
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
